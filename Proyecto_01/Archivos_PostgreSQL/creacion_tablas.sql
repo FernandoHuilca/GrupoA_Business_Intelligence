9134a6acf38694e92931a5da83abbab9c55b4892
sebas <sebastian11leon@gmail.com>
2026-05-27 20:37:57 -0500

    SQL raw table y csv

--- Proyecto_01/Archivos_PostgreSQL/creacion_tablas.sql
+++ Proyecto_01/Archivos_PostgreSQL/creacion_tablas.sql
@@ -1,59 +1,34 @@
 -- Creación de tablas en la base de datos
 
--- Tabla raw_desnutrition_infantil
-CREATE TABLE raw_desnutricion_infantil (
-    child_id VARCHAR(10),
-    gender CHAR(1),
-    age_months INT,
-    weight_kg NUMERIC(10,2),
-    height_cm NUMERIC(10,2),
-    nutritional_status VARCHAR(10),
-    region VARCHAR(20),
-    institution VARCHAR(20),
-    date_measured DATE
+-- Tabla raw_personas_desaparecidas
+CREATE TABLE raw_personas_desaparecidas (
+	fecha_desaparicion DATE,
+	fecha_denuncia DATE,
+	fecha_conocimiento DATE,
+	zona VARCHAR(50),
+	distrito VARCHAR(50),
+	circuito VARCHAR(50),
+	subcircuito VARCHAR(50),
+	codigo_provincia VARCHAR(5),
+	provincia VARCHAR(50),
+	codigo_canton VARCHAR(10),
+	canton VARCHAR(50),
+	dias_solucion INTEGER,
+	latitud_desaparicion varchar(100),
+	longitud_desaparicion varchar(100),
+	sexo VARCHAR(10),
+	nacionalidad VARCHAR(30),
+	edad INTEGER,
+	rango_edad VARCHAR(30),
+	etnia VARCHAR(30),
+	fecha_localizacion DATE,
+	latitud_localizacion varchar(100),
+	longitud_localizacion varchar(100),
+	provincia_localizacion VARCHAR(50),
+	situacion_actual VARCHAR(30),
+	motivo_desaparicion VARCHAR(100),
+	motivacion_desaparicion_observada VARCHAR(100),
+	estado_desaparecido VARCHAR(50)
 );
-
--- Tabla dim_child
-CREATE TABLE dim_child (
-	child_key SERIAL PRIMARY KEY,
-	child_id VARCHAR(10),
-	gender CHAR(1)
-);
-
--- Tabla dim_status
-CREATE TABLE dim_status(
-	status_key SERIAL PRIMARY KEY,
-	nutritional_status VARCHAR(10)
-);
-
--- Tabla de dim_location
-CREATE TABLE dim_location (
-	location_key SERIAL PRIMARY KEY,
-	region VARCHAR(50),
-	institution VARCHAR(100)
-);
-
--- Tabla dim_date
-CREATE TABLE dim_date (
-	date_key DATE PRIMARY KEY,
-	year int,
-	month int,
-	day int
-);
-
--- Tabla fact_measurement
-create table fact_measurement (
-  	record_key SERIAL PRIMARY KEY,
-  	child_key int,
-  	status_key int,
-  	location_key int,
-  	date_key date,
-	age_months int,
-	weight_kg numeric(10,2),
-	height_cm numeric(10,2)
-);
-
-
-
 
 
