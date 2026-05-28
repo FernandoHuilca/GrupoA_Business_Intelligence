99943229178acfaf25d209c04d998a6a7c97b150
mateonicolasg <mateonsg12@hotmail.com>
2026-05-27 21:29:21 -0500

    Añadiendo dim_fecha

--- Proyecto_01/Archivos_PostgreSQL/creacion_tablas.sql
+++ Proyecto_01/Archivos_PostgreSQL/creacion_tablas.sql
@@ -31,26 +31,23 @@ CREATE TABLE raw_personas_desaparecidas (
 	estado_desaparecido VARCHAR(50)
 );
 
-
 select * from raw_personas_desaparecidas
 
--- Tabla dim_estado
+-- Creación de la tabla: tabla dim_estado
 CREATE TABLE dim_estado(
 	estado_key serial primary key,
 	situacion_actual VARCHAR(30),
 	estado_desaparecido VARCHAR(50)
 )
 
--- Tabla dim_motivo
+-- Creación de la tabla: dim_motivo
 CREATE TABLE dim_motivo(
 	motivo_key serial primary key,
 	motivo_desaparicion VARCHAR(100),
 	motivacion_desaparicion_observada VARCHAR(100)
 )
 
-
-
--- Creacion de la tabla: dim_ubicacion_desaparicion
+-- Creación de la tabla: dim_ubicacion_desaparicion
 CREATE TABLE dim_ubicacion_desaparicion (
     ubicacion_desaparicion_key SERIAL PRIMARY KEY,
     zona VARCHAR(50),
@@ -63,10 +60,18 @@ CREATE TABLE dim_ubicacion_desaparicion (
 select * from dim_ubicacion_desaparicion
 
 
---Creacion de la tabla: dim_ubicacion_localizacion
+--Creación de la tabla: dim_ubicacion_localizacion
 CREATE TABLE dim_ubicacion_localizacion (
     ubicacion_localizacion_key SERIAL PRIMARY KEY,
     provincia_localizacion VARCHAR(100)
 );
 
 select * from dim_ubicacion_localizacion
+
+-- Creación de la tabla: dim_fecha
+CREATE TABLE dim_fecha(
+	fecha_key DATE PRIMARY KEY,
+	anio INTEGER,
+	mes INTEGER,
+	dia INTEGER
+);
